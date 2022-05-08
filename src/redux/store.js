1920061c9f202ddd67bf1947c0c21063b0632ea2
Joanna Tarnowska <joanna.tarnowska.91@gmail.com>
2022-05-08 20:20:37 +0200

    Make changes no. 3

--- src/redux/store.js
+++ src/redux/store.js
@@ -21,7 +21,7 @@ const reducer = (state, action) => {
     case 'ADD_CARD':
         return { ...state, cards: [...state.cards, { ...action.payload, id: shortid() }]};
     case 'UPDATE_SEARCHSTRING':
-        return { ...state, ...action.payload };
+        return { ...state, searchString: action.payload };
     default:
       return state;
   }
