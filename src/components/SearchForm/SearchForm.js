0d89bb4f71920b774894cda85148eb50acb89a99
Joanna Tarnowska <joanna.tarnowska.91@gmail.com>
2022-05-12 19:47:06 +0200

    Make changes in SearchForm & styles

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,21 +1,28 @@
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
-import { useDispatch } from 'react-redux';
-import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 import { updateSearchString } from '../../redux/store';
 
 const SearchForm = () => {
 
-  const [searchString, setSearchString] = useState('');
+  const phrase = useSelector(state => state.searchString)
+
+  const [searchString, setSearchString] = useState(phrase || '');
 
   const dispatch = useDispatch();
 
   const handleSubmit = e => {
     e.preventDefault();
     dispatch(updateSearchString(searchString));
-    setSearchString('');
   };
+
+  useEffect(() => {
+    return () => {
+    dispatch(updateSearchString(searchString));
+    }
+  }, []);
 
   return (
     <form className={styles.searchForm} onSubmit={handleSubmit}>
