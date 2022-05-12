85a9328606dee1c94eee2bf0108c1cca148a5bb1
Joanna Tarnowska <joanna.tarnowska.91@gmail.com>
2022-05-12 13:43:24 +0200

    Expand the app

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,18 @@
 import styles from './NavBar.module.scss';
 import Container from '../Container/Container';
+import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <nav>
       <Container>
         <div className={styles.navBar}>
-          <a href ="/"><span className={'fa fa-tasks'} /></a>
+          <Link to ="/"><span className={'fa fa-tasks'} /></Link>
           <ul className={styles.navlist}>
-            <li className={styles.navlink}><a href="/">HOME</a></li>
-            <li className={styles.navlink}><a href="/favorite">FAVORITE</a></li>
-            <li className={styles.navlink}><a href="/about">ABOUT</a></li>
+            <li className={styles.navlink}><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/">HOME</NavLink></li>
+            <li className={styles.navlink}><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/favorite">FAVORITE</NavLink></li>
+            <li className={styles.navlink}><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/about">ABOUT</NavLink></li>
           </ul>
         </div>
       </Container>
--- src/redux/store.js
+++ src/redux/store.js
@@ -9,10 +9,18 @@ export const getFilteredCards = ({ cards, searchString }, columnId) => cards
 
 export const getAllColumns = state => state.columns;
 
+export const getListById = ({ lists }, listId) => lists.find(list => list.id === listId);
+
+export const getColumnsByList = ({ columns }, listId) => columns
+  .filter(column => column.listId === listId);
+
+export const getAllLists = state => state.lists;
+
 // action creators
 export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
 export const addCard = payload => ({ type: 'ADD_CARD', payload});
-export const updateSearchString = payload => ({ type: 'UPDATE_SEARCHSTRING', payload})
+export const updateSearchString = payload => ({ type: 'UPDATE_SEARCHSTRING', payload});
+export const addList = payload => ({ type: 'ADD_LIST', payload });
 
 const reducer = (state, action) => {
   switch(action.type) {
@@ -22,6 +30,8 @@ const reducer = (state, action) => {
         return { ...state, cards: [...state.cards, { ...action.payload, id: shortid() }]};
     case 'UPDATE_SEARCHSTRING':
         return { ...state, searchString: action.payload };
+    case 'ADD_LIST':
+        return { ...state, lists: [...state.lists, { ...action.payload, id: shortid() }]};
     default:
       return state;
   }
